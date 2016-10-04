165348d8f9a2b481bf730af80314b71d7c31b27c
Frederick Beaulieu <frederick.beaulieu@hotmail.com>
2016-10-04 06:56:58 -0400

    Modified Scrubs_Creator.js for Harvesters

--- Scrubs_Creator.js
+++ Scrubs_Creator.js
@@ -1,3 +1,17 @@
-var title = "Scrubs first civilisationzzzzzz";
-console.log(title);
+
+module.exports.loop = function () {
+
+//Determine wich creep you need
+
+// var body="[WORK, CARRY, MOVE]";
+// var role="{role: 'harvester'}";
+
+// Determine if you can create a creep 
+
+if(Spawn1.canCreateCreep([WORK, CARRY, MOVE], null) == OK) {
+	//Create a creep
+	Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], null, {role: 'harvester'});
+}
+
+}
 
